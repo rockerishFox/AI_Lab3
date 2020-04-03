d0dd4150825a0b4e2e6b667b09a9f0d3412a03d6
RockerishFox <delia.csiki.dely@gmail.com>
2020-04-03 11:16:11 +0300

    added pdf with assignment specs

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from Utils import show_communities, print_solution
 def main():
     files = ["input/karate.gml", "input/dolphins.gml", "input/football.gml", "input/krebs.gml"]
     mine = ["input/lesmis.gml", "input/adjnoun.gml"]
-    serv = Service(mine[1])
+    serv = Service(mine[0])
     rez = serv.create_solution(150)
     print_solution(rez)
     show_communities(rez[2], rez[3])
